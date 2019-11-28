7868a8ca3528fac13d5409506f8377241e16bdfb
Peperooni <filoubiss@gmail.com>
2019-11-28 17:56:44 -0500

    createalbum
    debut create album

---
+++ HTML/createalbumview.php
@@ -0,0 +1,11 @@
+<div>
+<form>
+    <div>   </div>
+    <div class="form-group">
+        <label for="Media">Media</label>
+        <input type="file" class="form-control" name="Media" id="Media" required>
+        <div class="valid-feedback">Valid.</div>
+        <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+</form>
+</div>
