f9b0fe6270a3f2d9aeb17291796d104886a3cb57
Matis F <57538729+vex027@users.noreply.github.com>
2019-12-02 12:24:14 -0500

    Small change

--- HTML/imageview.php
+++ HTML/imageview.php
@@ -1,9 +1,31 @@
-<div class="container" style="margin-top:30px">
-    <?php
-        include "./CLASSES/IMAGE/imageTDG.php";
-        $albumId = $_POST["albumID"];
+<?php
+    include "./CLASSES/IMAGE/imageTDG.php";
+    //$albumId = $_POST["albumID"];
+ 
+    echo '<div class="container center" style="margin-top:30px">';
+        //<!-- Affiche limage Selectionner -->
+        echo '<div div class="border border-dark mb-sm 5">';
+        echo "<p>Noice</p>";
 
-        
 
-    ?>
-</div>
+
+        //<!--Upvote arrow & nb de UpVotes  -->
+        echo '<div class="d-flex flex-row bd-highlight border-top border-dark mb-sm 5">';
+        echo '<div class="p-2 bd-highlight  border-right border-dark mb-sm 5">Upvote button</div>';
+        echo '<div class="p-2 bd-highlight  border-right border-dark mb-sm 5">Likes</div>';
+        echo "</div>";
+
+        //<!--Commentaire section  -->
+        echo '<div div class="container border-top border-dark mb-sm 5">';
+        //Affichage des commentaire
+        echo "<p>Commentaire</p>";
+
+        echo "</div>";
+
+        echo '</div>';
+   
+        echo '</div>';
+?>
+<!-- upvote arrow -->
+<i class="fas fa-arrow-alt-circle-up"></i>
+    
