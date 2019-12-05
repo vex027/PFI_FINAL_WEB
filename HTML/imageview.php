da4797be90f8ef70204db4618633a4b532b3ad10
Matis F <57538729+vex027@users.noreply.github.com>
2019-12-05 16:37:46 -0500

    Update imageview.php

--- HTML/imageview.php
+++ HTML/imageview.php
@@ -1,23 +1,30 @@
 <div class="container center" style="margin-top:30px">
     <div div class="border border-dark mb-sm 5">
         <?php
-        include "./CLASSES/IMAGE/imageTDG.php";
-        $imageID = $_POST["imageID"];
+        include "./CLASSES/IMAGE/image.php";
+		$image = new image();
+        $image->load_image($_GET["id"]);
 
         //<!-- Affiche limage Selectionner -->
-        echo '<div><h1>'+ get_description_by_image()+'</h1></div>';
-        echo '<img src="ImageUrl" class="center">'+ load_image($imageID) + '</img>';
+        echo '<div><h1>'. $image->get_description() .'</h1></div>';
+        echo '<img src="' . $image->get_imageUrl() .'" class="center"></img>';
 
         //<!--Upvote arrow & nb de UpVotes  -->
         echo '<div class="d-flex flex-row bd-highlight border-top border-dark mb-sm 5">';
         echo '<div class="p-2 bd-highlight  border-right border-dark mb-sm 5"><i class="fas fa-arrow-alt-circle-up"></i></div>';
-        echo '<div class="p-2 bd-highlight  border-right border-dark mb-sm 5">Likes</div>';
+        echo '<div class="p-2 bd-highlight  border-right border-dark mb-sm 5">'. $image->get_likes() .'</div>';
         echo "</div>";
 
         //<!--Commentaire section  -->
         echo '<div div class="container border-top border-dark mb-sm 5">';
         //Affichage des commentaire
         echo "<p>Commentaire</p>";
+
+        echo "</div>";
+
+        echo '<div div class="container border-top border-dark mb-sm 5">';
+        //Affichage des commentaire
+        echo "<h1>Ajouter un commentaire</h1>";
 
         echo "</div>";
         ?>
