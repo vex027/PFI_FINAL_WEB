560513a55431c53df9b519bdb1669acdaf7e14d8
Matis F <57538729+vex027@users.noreply.github.com>
2019-12-05 17:28:21 -0500

    Update imageview.php

--- HTML/imageview.php
+++ HTML/imageview.php
@@ -1,13 +1,14 @@
-<div class="container center" style="margin-top:30px">
+<div class="container center mb-3" style="margin-top:30px">
     <div div class="border border-dark mb-sm 5">
         <?php
         include "./CLASSES/IMAGE/image.php";
+        include "./CLASSES/COMMENTAIRES/commentaire.php";
 		$image = new image();
         $image->load_image($_GET["id"]);
 
         //<!-- Affiche limage Selectionner -->
         echo '<div><h1>'. $image->get_description() .'</h1></div>';
-        echo '<img src="' . $image->get_imageUrl() .'" class="center"></img>';
+        echo '<img src="' . $image->get_imageUrl() .'" class="center mb-2" style="max-width:95%;height:auto;width:auto"></img>';
 
         //<!--Upvote arrow & nb de UpVotes  -->
         echo '<div class="d-flex flex-row bd-highlight border-top border-dark mb-sm 5">';
@@ -18,14 +19,14 @@
         //<!--Commentaire section  -->
         echo '<div div class="container border-top border-dark mb-sm 5">';
         //Affichage des commentaire
-        echo "<p>Commentaire</p>";
+        echo "<h2>Commentaire</h2>";
 
         echo "</div>";
 
         echo '<div div class="container border-top border-dark mb-sm 5">';
         //Affichage des commentaire
         echo "<h1>Ajouter un commentaire</h1>";
-
+        
         echo "</div>";
         ?>
         <form class method = "post" action = "./DOMAINLOGIC/ajoutercommentaire.dom.php?id=<?php echo $image->get_imageID()?>&type=IMG" enctype="multipart/form-data">
@@ -33,7 +34,7 @@
             <label for="commentaireIMG">Commentaire: </label>
             <textarea class="form-control" name="commentaireIMG" id="commentaireIMG" rows="3" id="commentaireIMG"></textarea>     
         </div>
-            <button class="btn btn-success" type="submit">Ajouter un commentaire</button>
+            <button class="btn btn-success mb-2" type="submit">Ajouter un commentaire</button>
         </form>
     </div>
 </div>
