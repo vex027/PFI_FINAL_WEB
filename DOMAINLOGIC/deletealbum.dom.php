edbb87d60811a4867bdb2a182e41ffb9d5076a3b
Peperooni <58051161+Peperooni@users.noreply.github.com>
2019-12-07 15:16:17 -0600

    update deleteimage et delete album

--- DOMAINLOGIC/deletealbum.dom.php
+++ DOMAINLOGIC/deletealbum.dom.php
@@ -1,18 +1,18 @@
 <?php  
     include "../CLASSES/ALBUM/album.php";
-    
-    $file_pointer = "../".$image->get_imageUrl();  
-    
-    if (!unlink($file_pointer)) {  
-    echo ("$file_pointer cannot be deleted due to an error");  
-    }  
-    else {  
-    echo ("$file_pointer has been deleted");  
-    } 
-    
+    $album = new Album();
+    $album->load_album($_POST["albumID"]);
+
+    $imageList = Image::create_image_list($album->get_id());
+
+    foreach($imageList as $image){
+        $image->delete_image();
+        $file_pointer = "../".$image->get_imageUrl(); 
+        unlink($file_pointer);
+         
+    }
 
     $albumID = $image->get_albumID();
     header("Location: ../album.php?id=$albumID");
     die();
-    
 ?>
--- DOMAINLOGIC/deleteimage.dom.php
+++ DOMAINLOGIC/deleteimage.dom.php
@@ -1,18 +1,18 @@
 <?php  
     include "../CLASSES/IMAGE/image.php";
-    $album = new Album();
-    $album->load_album($_POST["albumID"]);
+    $image = new Image();
+    $image->load_image($_GET["id"]);
+    $image->delete_image();
+    
+    $file_pointer = "../".$image->get_imageUrl();  
+    unlink($file_pointer);
 
-    $imageList = Image::create_image_list($album->get_id());
-
-    foreach($imageList as $image){
+    foreach($commentaireList as $image){
         $image->delete_image();
         $file_pointer = "../".$image->get_imageUrl(); 
         unlink($file_pointer);
          
     }
-
-    
 
     $albumID = $image->get_albumID();
     header("Location: ../album.php?id=$albumID");
