a699635eb230f7e452372a8678aecf9c5e7e9c77
Peperooni <58051161+Peperooni@users.noreply.github.com>
2019-12-07 15:11:10 -0600

    update deletealbum et deleteimage

--- DOMAINLOGIC/deleteimage.dom.php
+++ DOMAINLOGIC/deleteimage.dom.php
@@ -1,18 +1,17 @@
 <?php  
     include "../CLASSES/IMAGE/image.php";
-    $image = new Image();
-    $image->load_image($_GET["id"]);
-    $image->delete_image();
-    var_dump($image);
-    
-    $file_pointer = "../".$image->get_imageUrl();  
-    
-    if (!unlink($file_pointer)) {  
-    echo ("$file_pointer cannot be deleted due to an error");  
-    }  
-    else {  
-    echo ("$file_pointer has been deleted");  
-    } 
+    $album = new Album();
+    $album->load_album($_POST["albumID"]);
+
+    $imageList = Image::create_image_list($album->get_id());
+
+    foreach($imageList as $image){
+        $image->delete_image();
+        $file_pointer = "../".$image->get_imageUrl(); 
+        unlink($file_pointer);
+         
+    }
+
     
 
     $albumID = $image->get_albumID();
